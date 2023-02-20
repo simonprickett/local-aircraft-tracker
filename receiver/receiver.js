08b93f3ce918b286aa4393237e9a1052edc042d2
Simon Prickett <simon@redislabs.com>
2023-02-20 15:38:24 +0000

    Start of queuing logic.

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -8,6 +8,7 @@ const FLIGHT_RETENTION_PERIOD = 60 * 60; // 60 mins.
 const SBS_HOST = process.env.SBS_HOST || '127.0.0.1';
 const SBS_PORT = process.env.SBS_PORT || 30003;
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const FLIGHTAWARE_QUEUE = 'flightawarequeue';
 
 const sbs1Client = sbs1.createClient({
   host: SBS_HOST,
@@ -48,4 +49,18 @@ sbs1Client.on('message', async (msg) => {
 
   await redisClient.hSet(flightKey, msgData);
   redisClient.expire(flightKey, FLIGHT_RETENTION_PERIOD);
+
+  if (msgData.callsign) {
+    // Only push if we haven't seen this flight recently...
+    const response = await redisClient.set(`flightaware:recent:${msgData.callsign}`, msgData.last_updated, {
+      NX: true,
+      EX: 60 * 60
+    });
+
+    if (response) {
+      console.log(`Pushed Flightaware request for ${msgData.callsign}`);
+      // TODO replace with JSON containing callsign and hex ident...
+      redisClient.lPush(FLIGHTAWARE_QUEUE, msgData.callsign);
+    }
+  }
 });
