03c6478af257cb91092d33959d930495ced24ac5
Simon Prickett <simon@redislabs.com>
2023-02-20 17:28:14 +0000

    Removed aircraft_id field as it wasn't useful.

--- receiver/receiver.js
+++ receiver/receiver.js
@@ -5,6 +5,7 @@ import * as sbs1 from 'sbs1';
 dotenv.config()
 
 const FLIGHT_RETENTION_PERIOD = 60 * 60; // 60 mins.
+const FLIGHTAWARE_MAX_REQUEST_AGE = 60 * 60; // 60 mins.
 const SBS_HOST = process.env.SBS_HOST || '127.0.0.1';
 const SBS_PORT = process.env.SBS_PORT || 30003;
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
@@ -27,7 +28,6 @@ sbs1Client.on('message', async (msg) => {
 
   const msgData = {
     hex_ident: msg.hex_ident,
-    aircraft_id: msg.aircraft_id,
     last_updated: Date.now()
   };
 
@@ -52,15 +52,27 @@ sbs1Client.on('message', async (msg) => {
 
   if (msgData.callsign) {
     // Only push if we haven't seen this flight recently...
-    const response = await redisClient.set(`flightaware:recent:${msgData.callsign}`, msgData.last_updated, {
-      NX: true,
-      EX: 60 * 60
-    });
+    const response = await redisClient.set(
+      `flightaware:recent:${msgData.callsign}`, 
+      msgData.last_updated, 
+      {
+        NX: true,
+        EX: FLIGHTAWARE_MAX_REQUEST_AGE
+      }
+    );
 
     if (response) {
+      // We haven't seen a FlightAware API request for this identifier recently, 
+      // so send the hex_ident and callsign values to the enricher
+      // component - it needs callsign to pass to the FlightAware API and
+      // hex_ident to create the correct Redis key to store the data in...
+      const msgPayload = {
+        hex_ident: msgData.hex_ident,
+        callsign: msgData.callsign
+      };
+
       console.log(`Pushed Flightaware request for ${msgData.callsign}`);
-      // TODO replace with JSON containing callsign and hex ident...
-      redisClient.lPush(FLIGHTAWARE_QUEUE, msgData.callsign);
+      redisClient.lPush(FLIGHTAWARE_QUEUE, JSON.stringify(msgPayload));
     }
   }
 });
