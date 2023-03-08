6fa5372bf1077893b81c68ff6ebf64f39daed617
Simon Prickett <simon@redislabs.com>
2023-03-08 14:31:32 +0000

    Parses out fields to display on flipdot sign.

--- frontends/hanover-flipdot/flipdot-flights.js
+++ frontends/hanover-flipdot/flipdot-flights.js
@@ -1,3 +1,4 @@
+import FlipDot from 'flipdot-display';
 import * as dotenv from 'dotenv';
 import { createClient } from 'redis';
 
@@ -6,6 +7,7 @@ dotenv.config();
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 const SIGN_ROWS = parseInt(process.env.SIGN_ROWS, 10);
 const SIGN_COLS = parseInt(process.env.SIGN_COLS, 10);
+const SIGN_DEVICE = process.env.SIGN_DEVICE;
 const SIGN_ADDRESS = parseInt(process.env.SIGN_ADDRESS, 10);
 const SIGN_FLIP_INTERVAL = parseInt(process.env.SIGN_FLIP_INTERVAL, 10);
 const SIGN_REPEATS = parseInt(process.env.SIGN_REPEATS, 10);
@@ -16,7 +18,37 @@ const redisClient = createClient({
 
 await redisClient.connect();
 
+function calculatePixelWidth(msg) {
+  let pixelCount = 0;
+
+  for (const char of msg) {
+    // Regular chars 9 pixels wide, a space is 1.
+    pixelCount += (char === ' ' ? 1 : 9);
+  }
+
+  return pixelCount;
+}
+
+const flippy = new FlipDot(SIGN_DEVICE, SIGN_ADDRESS, SIGN_ROWS, SIGN_COLS);
+
+flippy.on('error', (err) => {
+  console.error('Flippy Error:');
+  console.error(err);
+});
+
+console.log('Listening for flights...');
 await redisClient.subscribe('interestingflights', (msg) => {
   const flightData = JSON.parse(msg);
   console.log(flightData);
+
+  const dataToDisplay = [
+    `${flightData.operator_iata}${flightData.flight_number}`,
+    `${flightData.origin_iata} - ${flightData.destination_iata}`,
+    flightData.aircraft_type,
+    flightData.registration,
+    `${flightData.altitude}FT`
+  ];
+
+  // TODO send this to the Hanover sign!
+  console.log(dataToDisplay);
 });
