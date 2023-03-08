30dde33b57ef60ac66c577ca8b8a6ea717066294
Simon Prickett <simon@redislabs.com>
2023-03-08 14:16:53 +0000

    Made more items configurable added basic pubsub subscriber.

--- frontends/hanover-flipdot/flipdot-flights.js
+++ frontends/hanover-flipdot/flipdot-flights.js
@@ -4,8 +4,19 @@ import { createClient } from 'redis';
 dotenv.config();
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
+const SIGN_ROWS = parseInt(process.env.SIGN_ROWS, 10);
+const SIGN_COLS = parseInt(process.env.SIGN_COLS, 10);
+const SIGN_ADDRESS = parseInt(process.env.SIGN_ADDRESS, 10);
+const SIGN_FLIP_INTERVAL = parseInt(process.env.SIGN_FLIP_INTERVAL, 10);
+const SIGN_REPEATS = parseInt(process.env.SIGN_REPEATS, 10);
 
-// TODO all the things!!!
-// Set up some env vars for port, address, rows, cols for the sign.
+const redisClient = createClient({
+  url: REDIS_URL
+});
 
-// Listen for pub/sub, display on the flipdot.
+await redisClient.connect();
+
+await redisClient.subscribe('interestingflights', (msg) => {
+  const flightData = JSON.parse(msg);
+  console.log(flightData);
+});
