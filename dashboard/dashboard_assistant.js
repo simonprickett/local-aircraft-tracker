e2a70ba11630460cf8185e011c38b7e6aea30bdf
Simon Prickett <simon@crudworks.org>
2025-12-22 14:57:01 +0000

    First cut of streaming position data.

--- dashboard/dashboard_assistant.js
+++ dashboard/dashboard_assistant.js
@@ -5,8 +5,13 @@ dotenv.config({ quiet: true });
 
 const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 const QUERY_INTERVAL = parseInt(process.env.QUERY_INTERVAL, 10);
-const AGGREGATE_COMMAND = [
-  "FT.AGGREGATE", "idx:flights", "*", "LOAD", "2", "__key", "@aircraft_type", "FILTER", "exists(@aircraft_type)", "SORTBY", "2", "@last_updated", "DESC", "LIMIT", "0", "1"
+const PLANE_POSITIONS_STREAM_KEY = "mappableflights";
+const LATEST_UPDATED_COMMAND = [
+  'FT.AGGREGATE', 'idx:flights', '*', 'LOAD', '2', '__key', '@aircraft_type', 'FILTER', 'exists(@aircraft_type)', 'SORTBY', '2', '@last_updated', 'DESC', 'LIMIT', '0', '1'
+];
+// TODO remove location hard coding!
+const PLANE_POSITIONS_COMMAND = [
+  'FT.AGGREGATE', 'idx:flights', '*', 'LOAD', '7', '@position', '@lat', '@lon', '@operator_iata', '@flight_number', '@origin_iata', '@destination_iata', 'FILTER', 'exists(@position)', 'FILTER', 'exists(@operator_iata)', 'FILTER', 'exists(@origin_iata)', 'FILTER', 'exists(@destination_iata)', 'APPLY', 'geodistance(@position, "-1.148369,52.953150")', 'AS', 'dist', 'LIMIT', '0', '9999', 'SORTBY', '2', '@dist', 'ASC'
 ];
 
 // Sleep for QUERY_INTERVAL milliseconds.
@@ -23,13 +28,45 @@ const redisClient = createClient({
 await redisClient.connect();
 
 while(true) {
-  const response = await redisClient.sendCommand(AGGREGATE_COMMAND);
+  let response = await redisClient.sendCommand(LATEST_UPDATED_COMMAND);
 
-  console.log(response[1])
   if (response && response.length == 2 && response[1].length === 6) {
     const redisKey = response[1][1];
     console.log(`Found latest updated flight with enriched data: ${redisKey}`);
     await redisClient.copy(redisKey, `lookup:latest_enriched_flight`, { REPLACE: true });
+  }
+
+  // This is pretty naive, we could improve by sending commands in an explicit pipeline.
+  response = await redisClient.sendCommand(PLANE_POSITIONS_COMMAND);
+  console.log(`Found ${response[0]} nearby flights with enriched data.`);
+
+
+  if (response && response[0] > 0) {
+    // First item is the count, remove it.
+    response.shift();
+
+    // Delete any old stream.
+    await redisClient.del(PLANE_POSITIONS_STREAM_KEY);
+
+    for (const thisFlight of response) {
+      const flightObj = {};
+      for (let n = 0  ; n < thisFlight.length; n+=2) {
+        const key = thisFlight[n];
+        const value = thisFlight[n+1];
+        flightObj[key] = value;
+      }
+
+      delete flightObj.position; // No need to store this in the stream.
+
+      flightObj['description'] = `${flightObj.operator_iata}${flightObj.flight_number}: ${flightObj.origin_iata}-${flightObj.destination_iata}`;
+
+      // TODO: Consider operator color, aircraft type.
+
+      console.log(flightObj);
+
+      // Add to stream, no need to await this as the order doesn't matter.
+      redisClient.xAdd(PLANE_POSITIONS_STREAM_KEY, '*', flightObj);
+    }  
   }
 
   await sleep();
